d84c14155c9d0a045dda5d286887352542e0864e
rodrigo-prado <rodrigo_prado@id.uff.br>
2018-10-17 14:43:42 -0300

    Add files via upload
    Adding files

---
+++ python3/1_beginner/1001.py
@@ -0,0 +1,3 @@
+a = int(input())
+b = int(input())
+print 'X = ' + str(a + b)
---
+++ python3/1_beginner/1008.py
@@ -0,0 +1,5 @@
+n = int(input())
+wh = int(input())
+ph = float(input())
+s = (wh*ph)
+print("NUMBER = {0}\nSALARY = U$ {1:.2f}".format(n, s))
